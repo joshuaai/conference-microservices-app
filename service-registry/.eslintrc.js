bccd0a41fd3478e037f1fcfdba36fdce18cfdec5
joshuaai <joshuaiheji@gmail.com>
2020-01-02 14:45:53 +0100

    update service registry dependencies

--- service-registry/.eslintrc.js
+++ service-registry/.eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    "linebreak-style": 0
-  },
-};
+    env: {
+        browser: true,
+        es6: true
+    },
+    extends: 'eslint',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        'arrow-body-style': 0,
+        quotes: 0,
+        semi: 0,
+        'node/shebang': 0,
+        'no-unused-vars': 'warn'
+    }
+}
